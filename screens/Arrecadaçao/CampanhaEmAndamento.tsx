7ff09253582c7fd3316659ae1fdc88cfedfeb183
Italo Matos <italo.matos@olxbr.com>
2024-11-01 20:20:01 -0300

    card de resumo de campanha

--- screens/Arrecadaçao/CampanhaEmAndamento.tsx
+++ screens/Arrecadaçao/CampanhaEmAndamento.tsx
@@ -1,193 +1,163 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Image } from 'react-native';
-import { Appbar, Divider, FAB, Icon, Surface, Text, TextInput } from 'react-native-paper';
-import { Button } from 'react-native-paper';
-import { StatusBar } from 'react-native';
-import { ScrollView } from 'react-native';
-import { Dimensions } from 'react-native';
-import { months } from '@/utils/utils';
-import dayjs from 'dayjs';
-import DateTimePicker, {
-    DateTimePickerAndroid,
-    DateTimePickerEvent,
-} from '@react-native-community/datetimepicker';
-import { Avatar, Card, IconButton } from 'react-native-paper';
+import React from 'react';
+import { StyleSheet, View, StatusBar, ScrollView, Dimensions } from 'react-native';
+import { Appbar, Divider, Surface, Text, Button, IconButton, Chip, Icon } from 'react-native-paper';
+import InfoCategoria from './InfoCategoria';
 
 const vh = Dimensions.get('window').height / 100;
 
-export default function CampanhaEmAndamento({
-    navigation,
-    route,
-}: {
+type CampanhaEmAndamentoProps = {
     navigation: any;
     route: any;
-}) {
-    const currentDate = new Date();
-    const todayDate = `${currentDate.getDate()}/${
-        months[currentDate.getMonth()]
-    }/${currentDate.getFullYear()}`;
-    const [name, setName] = useState(`Campanha de arrecadação ${todayDate}`);
+};
 
-    const handleName = (text: string) => {
-        setName(text);
-    };
-
-    const [date, setDate] = useState(new Date());
-
-    const onChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
-        const currentDate = selectedDate || new Date();
-        setDate(currentDate);
-    };
-
-    const showMode = (currentMode: any) => {
-        DateTimePickerAndroid.open({
-            value: date,
-            onChange,
-            mode: currentMode,
-            is24Hour: true,
-        });
-    };
-
-    const showDatepicker = () => {
-        showMode('date');
-    };
-
-    const formattedDate = dayjs(date).format('DD/MM/YYYY');
-
-    const newCampaignPayload = {
-        name: name,
-        startDate: date,
-        endDate: null,
-    };
-
-    const createNewCampaign = () => {
-        console.log('Creating new campaign with payload: ', newCampaignPayload);
-    };
+export default function CampanhaEmAndamento({ navigation, route }: CampanhaEmAndamentoProps) {
+    const mockData = [
+        {
+            category: 'Feijão',
+            quantity: 1000,
+            packages: 80,
+        },
+        {
+            category: 'Arroz',
+            quantity: 1000,
+            packages: 80,
+        },
+        {
+            category: 'Macarrão',
+            quantity: 1200,
+            packages: 120,
+        },
+        {
+            category: 'Açúcar',
+            quantity: 1000,
+            packages: 80,
+        },
+    ];
 
     return (
-        <View style={{ flex: 1 }}>
+        <>
             <StatusBar barStyle="dark-content" />
 
             <Appbar.Header mode="center-aligned" elevated>
                 <Appbar.BackAction onPress={() => navigation.navigate('ArrecadacaoTelaInicial')} />
                 <Appbar.Content title="Nova campanha" />
             </Appbar.Header>
-
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    flexDirection: 'column',
-                    flex: 1,
-                }}
-            >
-                <Text>Campanha em andamento</Text>
-                <Surface
-                    style={{
-                        width: '90%',
-                        height: 300,
-                        margin: 20,
-                        borderRadius: 10,
-                        justifyContent: 'center',
-                    }}
+            <View style={styles.container}>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={styles.scrollContent}
                 >
-                    <Text
-                        variant="titleMedium"
-                        style={{
-                            margin: 10,
-                        }}
-                    >
-                        Registrar doação
-                    </Text>
-                    <Divider />
-
-                    <View
-                        style={{
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            alignContent: 'center',
-                            margin: 10,
-                            width: '100%',
-                        }}
-                    >
-                        <Button
-                            mode="contained-tonal"
-                            onPress={() => console.log('Pressed escanear código de barras')}
-                            style={{
-                                margin: 'auto',
-                                justifyContent: 'center',
-                                borderRadius: 100,
-                            }}
-                            contentStyle={{
-                                width: 150,
-                                height: 150,
-                            }}
-                        >
-                            <View
-                                style={{
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    alignContent: 'center',
-                                }}
+                    <Surface style={styles.surface}>
+                        <Divider />
+                        <View style={styles.innerContainer}>
+                            <IconButton
+                                icon="barcode-scan"
+                                mode="contained"
+                                size={100}
+                                style={styles.iconButton}
+                                onPress={() => console.log('Pressed escanear código de barras')}
+                            />
+                            <Button
+                                mode="contained"
+                                onPress={() => console.log('Pressed Escanear código de barras')}
+                                style={styles.scanButton}
                             >
-                                <Icon source="barcode-scan" size={100} />
-                            </View>
-                        </Button>
-                        <Button
-                            mode="contained"
-                            onPress={() => console.log('Pressed Escanear código de barras')}
-                            style={{
-                                width: '80%',
-                                margin: 10,
-                                borderRadius: 10,
-                            }}
-                        >
-                            Escanear código de barras
-                        </Button>
-                    </View>
-                </Surface>
+                                Escanear código de barras
+                            </Button>
+                        </View>
+                    </Surface>
 
-                {/* <Card style={{ width: '90%' }}>
-                    <Card.Actions>
-                        <Card.Title
-                            title="Escanear código de barras"
-                            subtitle="Card Subtitle"
-                            left={(props) => <Avatar.Icon {...props} icon="folder" />}
-                        />
-                    </Card.Actions>
-                </Card> */}
-            </ScrollView>
-        </View>
+                    <Surface style={styles.surface}>
+                        <View style={styles.header}>
+                            <Text variant="titleMedium" style={styles.title}>
+                                Campanha 1
+                            </Text>
+                            <Chip
+                                icon={() => (
+                                    <Icon source="progress-clock" size={16} color="black" />
+                                )}
+                                style={{ backgroundColor: '#81c784' }}
+                                compact={true}
+                                textStyle={{ color: 'black' }}
+                            >
+                                Em andamento
+                            </Chip>
+                        </View>
+
+                        <Divider />
+
+                        <View style={styles.innerContainerCategory}>
+                            <Surface style={styles.categoryContainer}>
+                                {mockData.map((data, index) => (
+                                    <InfoCategoria
+                                        key={index}
+                                        category={data.category}
+                                        quantity={data.quantity}
+                                        packages={data.packages}
+                                    />
+                                ))}
+                            </Surface>
+                        </View>
+                    </Surface>
+                </ScrollView>
+            </View>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
-    containerMain: {
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
+    container: {
         flex: 1,
     },
-    formInputs: {
-        width: '85%',
-    },
-    inputButtons: {
+    scrollContent: {
         alignItems: 'center',
+        flexDirection: 'column',
+    },
+    surface: {
+        width: '95%',
+        margin: 20,
+        borderRadius: 10,
         justifyContent: 'center',
     },
-    buttonNewCampaign: {
-        marginTop: 20,
-        borderRadius: 10,
-        width: '90%',
-    },
-    textNewCampaign: {
-        color: 'white',
-        fontWeight: '700',
-        alignContent: 'center',
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center',
+        margin: 10,
+        color: 'black',
     },
-    calendarContainer: {
-        backgroundColor: '#FFFFFF',
+    title: {
+        margin: 5,
+    },
+    innerContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+    },
+    iconButton: {
+        height: 150,
+        width: 150,
+        borderRadius: 100,
+    },
+    scanButton: {
+        width: '80%',
+        margin: 10,
+        borderRadius: 10,
+    },
+    categoryContainer: {
+        width: '100%',
+        elevation: 0,
+        borderRadius: 10,
+        borderTopRightRadius: 5,
+        borderTopLeftRadius: 5,
+        borderTopWidth: 0,
+    },
+    innerContainerCategory: {
+        alignItems: 'center',
+        borderRadius: 10,
+    },
+    chip: {
+        backgroundColor: '#81c784',
+        marginVertical: 4,
     },
 });
